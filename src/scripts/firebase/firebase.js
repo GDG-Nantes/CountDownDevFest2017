bd2d5b75176482923340275fde54c4665186148a
jefBinomed <jean.francois.garreau@gmail.com>
2017-09-22 22:34:08 +0200

    update version and app url

--- src/scripts/firebase/firebase.js
+++ src/scripts/firebase/firebase.js
@@ -3,19 +3,18 @@
 /**
  * Basic Firebase helper
  */
-export class FireBaseLegoApp{
-    constructor(){
+export class FireBaseLegoApp {
+    constructor() {
         // Configuration of the application, You should update with your Keys !
         this.config = {
             apiKey: "AIzaSyDr9R85tNjfKWddW1-N7XJpAhGqXNGaJ5k",
-            authDomain: "legonnary.firebaseapp.com",
-            databaseURL: "https://legonnary.firebaseio.com",
+            authDomain: "devfest-draw.firebaseapp.com",
+            databaseURL: "https://devfest-draw.firebaseio.com",
             storageBucket: "",
-        } 
+        }
 
         this.app = firebase.initializeApp(this.config);
     }
 
 
 }
-
